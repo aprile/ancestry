cea7720b71944bb40a2132cbf3721162805b2712
aprileelcich <aprile.elcich@gmail.com>
2020-01-16 14:41:52 +0100

    wip

--- src/gedcom-reader/mixins/names.js
+++ src/gedcom-reader/mixins/names.js
@@ -4,21 +4,29 @@ function getName (el) {
   if (!el.tree) return
 
   const node = getTags(el.tree, 'NAME')[0]
+  if (!node) return
+
   const o = {}
+  const nameData = node.data
 
-  if (node) {
-    o.data = node.data
+  o.data = nameData
 
-    if (node.tree) {
-      const fName = getTags(node.tree, 'GIVN')[0]
-      const lName = getTags(node.tree, 'SURN')[0]
-      const nName = getTags(node.tree, 'NICK')[0]
+  if (node.tree) {
+    const fName = getTags(node.tree, 'GIVN')[0]
+    const lName = getTags(node.tree, 'SURN')[0]
+    const nName = getTags(node.tree, 'NICK')[0]
 
-      if (fName) o.givn = fName.data
-      if (lName) o.surn = lName.data
-      if (nName) o.nick = nName.data
-    }
+    if (fName) o.givn = fName.data
+    if (lName) o.surn = lName.data
+    if (nName) o.nick = nName.data
+  } else if (nameData) {
+    const [fName, lName] = nameData.split('/').map(n => n.trim())
+    const p = { givn: fName, surn: lName }
+    const merged = Object.assign(p, o)
+
+    return merged
   }
+
   return o
 }
 
--- src/translator/mixins/personName.js
+++ src/translator/mixins/personName.js
@@ -2,13 +2,20 @@ function personName (data) {
   if (data.tag && data.tag !== 'INDI') return
 
   const person = data
+  let result = ''
 
   if (person.name) {
     const fName = person.name.givn
     const lName = person.name.surn
+    const nameData = person.name.data
 
-    if (fName && lName) return `${fName} ${lName}`
-    else return person.name.data
+    if (fName && lName) result += fName + ' ' + lName
+    else if (nameData) result += nameData
+    else if (!nameData && fName) result += fName
+    else if (!nameData && lName) result += lName
+    else result = 'Unknown'
+
+    return result
   }
 }
 
